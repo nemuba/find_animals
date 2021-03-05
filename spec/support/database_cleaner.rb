db9e8644a69f40f39fa7d8caa8a718e7c24505c3
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-03-05 11:59:30 -0300

    setup initial

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,14 @@
+require 'database_cleaner/active_record'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |teste|
+    DatabaseCleaner.cleaning do
+      teste.run
+    end
+  end
+end
---
+++ spec/support/shoulda_matchers.rb
@@ -0,0 +1,7 @@
+# Config Shoulda Matchers specs
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
