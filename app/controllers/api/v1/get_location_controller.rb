e2a26395a9e27c207e217d0f43fb670dda328111
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-04-13 15:55:38 -0300

    fix error get_location

--- app/controllers/api/v1/get_location_controller.rb
+++ app/controllers/api/v1/get_location_controller.rb
@@ -11,18 +11,19 @@ module Api
           ]
         }, status: :unprocessable_entity unless params_valid?
 
-        address = Geocoder.search([params[:longitude], params[:latitude]]).first
+        byebug
+        address = Geocoder.search([params[:longitude], params[:latitude]])
 
-        render json: { msg: "Location not found " } unless address.present?
+        return render json: { msg: "Location not found " } unless address.present?
 
         data = {
           location: {
-            street: address.data["address"]["road"],
-            neighborhood: address.data["address"]["suburb"],
-            city: address.data["address"]["town"],
-            state: address.data["address"]["state"],
-            country: address.data["address"]["country"],
-            zipcode: address.data["address"]["postcode"]
+            street: address.first.data["address"]["road"],
+            neighborhood: address.first.data["address"]["suburb"],
+            city: address.first.data["address"]["town"],
+            state: address.first.data["address"]["state"],
+            country: address.first.data["address"]["country"],
+            zipcode: address.first.data["address"]["postcode"]
           }
         }
 
